fb035a68a41deaad3374bdadc584aab969640a7d
alena195101 <alena195101@yandex.ru>
2020-04-30 23:53:32 +0300

    Update tree_goods.sql

--- tree_goods.sql
+++ tree_goods.sql
@@ -1,14 +1,9 @@
 with RECURSIVE t as (
-	select gr.id, gr.name, gr.parent, gg.name parent_name
-	from client_groups gr join client_groups gg on gg.id = gr.parent
+	select g.id, g.name, g.parent, gp.name parent_name
+	from goods_groups g join goods_groups gp on gp.id = g.parent
 	union all 
-		  select gg.id, gg.name, t.parent, gp.name from client_groups as gg 
+		  select gg.id, gg.name, t.parent, gp.name from goods_groups as gg 
 			join t on t.id = gg.parent
-			join client_groups gp on gp.id = t.parent)
+			join goods_groups gp on gp.id = t.parent)
 
-select distinct cl.id client_id, cl.name client_name, cl.client_groups groups_id, g.name group_name
-	from client cl join client_groups g on cl.client_groups = g.id
-union all
-select cl.id, cl.name, t.parent, t.parent_name from client cl
-	join t on t.id = cl.client_groups
-order by client_id;
+select * from t;
